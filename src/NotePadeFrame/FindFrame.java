7c3835175505289e1a925f8ca03d9e44e030072d
Daniel.G.Pavlov <daniel.g.pavlov@gmail.com>
2019-03-28 12:36:22 +0200

    FontFrame

--- src/NotePadeFrame/FindFrame.java
+++ src/NotePadeFrame/FindFrame.java
@@ -1,6 +1,5 @@
 package NotePadeFrame;
 
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
--- src/NotePadeFrame/FontFrame.java
+++ src/NotePadeFrame/FontFrame.java
@@ -2,21 +2,21 @@ package NotePadeFrame;
 
 import javax.swing.*;
 
-public class AboutFrame  extends JFrame {
+public class FontFrame  extends JFrame {
 
     public JPanel panel = new JPanel();
     public JLabel label = new JLabel();
     public JButton button = new JButton();
 
-    public AboutFrame() {
+    public FontFrame() {
 
-        setTitle("About NotePad");
-        setSize(400, 500);
+        setTitle("Font");
+        setSize(400, 600);
         setLocation(350, 80);
         panel.setLayout(null);
         getContentPane().add(panel);
 
-        setDefaultCloseOperation(AboutFrame.DISPOSE_ON_CLOSE);
+        setDefaultCloseOperation(FontFrame.DISPOSE_ON_CLOSE);
         setResizable(false);
         setVisible(true);
         repaint();
